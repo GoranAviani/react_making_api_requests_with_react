63ec4fcc4b7fb1a2f53efc22a1c32213c9277ed2
GoranAviani <goranaviani@gmail.com>
2022-04-04 10:23:38 +0200

    react_making_api_requests_with_react

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,24 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {images: []}
+    }
 
-    async onSearchSubmit(term) {
+    async onSearchSubmit (term) {
         const response = await axios.get('https://api.unsplash.com/search/photos',
             {
                 params: {
                     query: term
                 },
                 headers: {
-                    Authorization: 'Client-ID Cbo29MbIl9rNunrb5vdOQHYpZ3E_JvuNa_Fcjy-OpkI'
+                    Authorization: 'Client-ID '
                 }
             });
+        console.log(this)
+        this.setState({images: response.data.results})
 
-        console.log(response.data.results)
 
     }
 
@@ -26,7 +31,9 @@ class App extends React.Component {
         return (
             <div>
                 <div className="ui container" style={{marginTop: '10px'}}>
-                    <SearchBar onSubmit={this.onSearchSubmit}/>
+                    <SearchBar onSubmit={this.onSearchSubmit}
+                    guesswhatIAm = "im a props object"/>
+                    Found: {this.state.images.length} images!
                 </div>
             </div>
         )
