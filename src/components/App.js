9f7d181e11e039885741ca9d48644dae51328a94
GoranAviani <goranaviani@gmail.com>
2022-04-04 10:08:17 +0200

    react_making_api_requests_with_react

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,20 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
 
-    onSearchSubmit(term){
+    onSearchSubmit(term) {
 
 // Make a request for a user with a given ID
-axios.get('https://api.unsplash.com/search/photos',
-    {
-        params: {
-          query: term
-        },
-        headers: {
-            Authorization: 'Client-ID Cbo29MbIl9rNunrb5vdOQHYpZ3E_JvuNa_Fcjy-OpkI'
-        }
-    }).then((response)=>{
-        console.log(response.data.results)
-});
+        axios.get('https://api.unsplash.com/search/photos',
+            {
+                params: {
+                    query: term
+                },
+                headers: {
+                    Authorization: 'Client-ID Cbo29MbIl9rNunrb5vdOQHYpZ3E_JvuNa_Fcjy-OpkI'
+                }
+            }).then((response) => {
+            console.log(response.data.results)
+        });
     }
 
 
@@ -27,7 +27,7 @@ axios.get('https://api.unsplash.com/search/photos',
         return (
             <div>
                 <div className="ui container" style={{marginTop: '10px'}}>
-                    <SearchBar onSubmit = {this.onSearchSubmit}/>
+                    <SearchBar onSubmit={this.onSearchSubmit}/>
                 </div>
             </div>
         )
